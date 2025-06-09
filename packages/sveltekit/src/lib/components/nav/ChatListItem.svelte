2819c25c101da8f5eb8f9a832d96204e79a9ba05
ezShroom <68300730+ezShroom@users.noreply.github.com>
2025-06-09 11:05:20 +0100

    tchatter

--- packages/sveltekit/src/lib/components/nav/ChatListItem.svelte
+++ packages/sveltekit/src/lib/components/nav/ChatListItem.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import clsx from 'clsx'
 	import type { Snippet } from 'svelte'
+	import { Pencil, Trash } from 'svelte-bootstrap-icons'
 
 	const {
 		selected,
@@ -11,10 +12,15 @@
 
 <button
 	class={clsx(
-		'flex w-full cursor-pointer justify-center gap-2 rounded-xl border p-2 transition-all hover:scale-95 hover:bg-stone-900',
+		'flex w-full cursor-pointer justify-center gap-2 rounded-xl border p-2 text-left transition-all hover:scale-95 hover:bg-stone-900',
 		selected && 'border-stone-800 bg-stone-950',
 		!selected && 'border-transparent'
 	)}
 	{onclick}
-	>{@render children?.()}
+	><div class="grow">
+		<!--<div class="absolute right-0 z-10 size-4 h-full bg-gradient-to-r from-black/0 to-black"></div>-->
+		{@render children?.()}
+	</div>
+	<Pencil class="my-auto" />
+	<Trash class="my-auto" />
 </button>
