17cf42513d7b3f679e310dfb1e9739a83442f58c
ezShroom <68300730+ezShroom@users.noreply.github.com>
2025-06-10 08:54:16 +0100

    begin handling message

--- packages/sveltekit/src/lib/components/root/GlobalRoot.svelte
+++ packages/sveltekit/src/lib/components/root/GlobalRoot.svelte
@@ -1,11 +1,18 @@
-<script>
+<script lang="ts">
+	import SuperJSON from 'superjson'
 	import ChatRoot from './ChatRoot.svelte'
+	import {
+		isDownstreamWsMessage,
+		type DownstreamWsMessage
+	} from 'shared/src/types/ws/downstream/DownstreamWsMessage'
 
 	let ws
 	$effect(() => {
 		ws = new WebSocket('ws://localhost:8787/session')
 		ws.onmessage = (event) => {
-			console.log(event.data)
+			const message: DownstreamWsMessage = SuperJSON.parse(event.data)
+			if (!isDownstreamWsMessage(message))
+				return console.error('Invalid message received!\n', event.data)
 		}
 	})
 </script>
