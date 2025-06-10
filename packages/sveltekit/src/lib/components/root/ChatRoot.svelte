8b544bfce27cead92fe7a0c6a5da41e78695cf1a
ezShroom <68300730+ezShroom@users.noreply.github.com>
2025-06-10 06:45:47 +0100

    improved chat design

--- packages/sveltekit/src/lib/components/root/ChatRoot.svelte
+++ packages/sveltekit/src/lib/components/root/ChatRoot.svelte
@@ -6,10 +6,10 @@
 	<Navigation />
 	<main class="flex grow flex-col bg-stone-950 text-white">
 		<div class="mx-auto size-full max-w-256 grow p-4">
-			<div class="ml-auto max-w-128 rounded-xl rounded-br-none bg-stone-900 p-4">
-				What is Rizz Chat?
+			<div class="flex w-full justify-end">
+				<div class="max-w-128 rounded-xl rounded-br-none bg-stone-900 p-4">What is Rizz Chat?</div>
 			</div>
-			<div>Rizz Chat is the best!</div>
+			<div class="p-4">Rizz Chat is the best!</div>
 		</div>
 		<input
 			class="appearance-none !border-none bg-stone-900 p-4 !outline-none placeholder:text-stone-400"
