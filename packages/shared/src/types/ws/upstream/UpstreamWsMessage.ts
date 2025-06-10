93d386970565dd1a6dd5078c16eb68d2543149ac
ezShroom <68300730+ezShroom@users.noreply.github.com>
2025-06-10 21:15:04 +0100

    attempt (and fail) to fix monorepo woes (i now know turborepo is the way)
    add submit to the discriminatedUnion

--- packages/shared/src/types/ws/upstream/UpstreamWsMessage.ts
+++ packages/shared/src/types/ws/upstream/UpstreamWsMessage.ts
@@ -1,11 +1,21 @@
 import { z } from 'zod/v4'
 import { UpstreamWsMessageAction } from './UpstreamWsMessageAction'
 import semver from 'semver'
+import { ReasoningLevel } from '../../ReasoningLevel'
 
 export const UpstreamWsMessageSchema = z.discriminatedUnion('action', [
 	z.object({
 		action: z.literal(UpstreamWsMessageAction.Hello),
 		version: z.string().refine(semver.valid)
+	}),
+	z.object({
+		action: z.literal(UpstreamWsMessageAction.Submit),
+		thread: z.uuidv7(),
+		attachments: z.array(z.url()).optional(),
+		body: z.string().max(10_000),
+		model: z.uuidv7(),
+		reasoningLevel: z.enum(ReasoningLevel),
+		search: z.boolean()
 	})
 ])
 export type UpstreamWsMessage = z.infer<typeof UpstreamWsMessageSchema>
