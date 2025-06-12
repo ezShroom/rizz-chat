827416cfb69bfe5884122888638cd4ef2b0131f4
ezShroom <68300730+ezShroom@users.noreply.github.com>
2025-06-12 19:27:29 +0100

    accept suggestion

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,4 +1,6 @@
 import { $ } from 'bun'
 
-if (process.platform === 'win32') console.log('The Turso CLI does not run on Windows')
-else await $`turso dev --db-file ./dev.db`
+if (process.platform === 'win32') {
+	console.error('The Turso CLI does not run on Windows')
+	process.exit(1)
+} else await $`turso dev --db-file ./dev.db`
