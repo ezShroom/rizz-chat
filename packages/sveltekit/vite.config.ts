16d1e9eafccab38815960431a314c238e1de5db5
ezShroom <68300730+ezShroom@users.noreply.github.com>
2025-06-18 10:31:40 +0100

    add worker plugin (apparently they use separate plugins! odd but ok)

--- packages/sveltekit/vite.config.ts
+++ packages/sveltekit/vite.config.ts
@@ -1,23 +1,23 @@
 import tailwindcss from '@tailwindcss/vite'
 import { sveltekit } from '@sveltejs/kit/vite'
-import { defineConfig } from 'vite'
+import { defineConfig, type PluginOption } from 'vite'
 import devtoolsJson from 'vite-plugin-devtools-json'
 
+const sqlRawPlugin: PluginOption = {
+	name: 'vite-plugin-sql-raw',
+	enforce: 'pre',
+	transform(code, id) {
+		if (id.endsWith('.sql')) {
+			return `export default \`${code.replaceAll('`', '\\`')}\`;`
+		}
+	}
+}
+
 export default defineConfig({
-	plugins: [
-		{
-			name: 'vite-plugin-sql-raw',
-			enforce: 'pre',
-			transform(code, id) {
-				if (id.endsWith('.sql')) {
-					return `export default \`${code.replaceAll('`', '\\`')}\`;`
-				}
-			}
-		},
-		tailwindcss(),
-		sveltekit(),
-		devtoolsJson()
-	],
+	plugins: [sqlRawPlugin, tailwindcss(), sveltekit(), devtoolsJson()],
+	worker: {
+		plugins: () => [sqlRawPlugin]
+	},
 	optimizeDeps: {
 		exclude: ['wa-sqlite']
 	},
